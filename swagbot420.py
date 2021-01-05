ef218245bdfcd5817a9fe075c375d064020bdbc5
404NameN0tF0und <75540455+404NameN0tF0und@users.noreply.github.com>
2021-01-05 22:48:40 +0000

    initial commit

---
+++ swagbot420.py
@@ -0,0 +1,22 @@
+import discord
+import random
+
+text = ['fuck you','retard','shut up','ur parents dont love u','why do u bother living','u have no friends','suck a dick','https://logal.media/vnULVo_IsK.gif','ur whole family is dissapointed in u','u have more fingers than iq','stupid bitch','ok airplane']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    print(client.user.mention)
+
+    if client.user in message.mentions:
+        await message.channel.send(random.choice(text))
+
+client.run('bot token here')
