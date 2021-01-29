f715b9fccf5681f9d09a11e7292e4836cbe8bd05
Logan Fick <logan@logal.dev>
2021-01-29 17:03:56 -0500

    Added new insult.

--- swagbot420.py
+++ swagbot420.py
@@ -1,7 +1,7 @@
 import discord
 import random
 
-text = ['fuck you','retard','shut up','ur parents dont love u','why do u bother living','u have no friends','suck a dick','https://logal.media/vnULVo_IsK.gif','ur whole family is dissapointed in u','u have more fingers than iq','stupid bitch','ok airplane','ur ancestry incestry']
+text = ['fuck you','retard','shut up','ur parents dont love u','why do u bother living','u have no friends','suck a dick','https://logal.media/vnULVo_IsK.gif','ur whole family is dissapointed in u','u have more fingers than iq','stupid bitch','ok airplane','ur ancestry incestry','https://logal.media/aHrVwW1YQ.jpg']
 
 client = discord.Client()
 
